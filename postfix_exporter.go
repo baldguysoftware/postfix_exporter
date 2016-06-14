cd9ea03fbb4ef880dd9bcb831d391ff185156eef
therealbill <therealbill@me.com>
2016-06-13 21:51:58 -0400

    removed shell calls, added queue_root configurable
    Using Go's native filesystem capabilities rather than calling find.

--- postfix_exporter.go
+++ postfix_exporter.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
-        "os/exec"
+	"os"
 	"sync"
-        "strings"
-        "strconv"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/log"
@@ -18,124 +17,131 @@ const (
 
 var (
 	listenAddress = flag.String("telemetry.address", ":9115", "Address on which to expose metrics.")
-	metricsPath  = flag.String("telemetry.endpoint", "/metrics", "Path under which to expose metrics.")
+	metricsPath   = flag.String("telemetry.endpoint", "/metrics", "Path under which to expose metrics.")
+	queueDir      = flag.String("postfix.queue_root", "/var/spool/postfix", "Path to Postfix queue directories")
 )
 
 // Exporter collects postfix stats from machine of a specified user and exports them using
 // the prometheus metrics package.
 type Exporter struct {
-	mutex          sync.RWMutex
-        totalQ         prometheus.Gauge
-        incomingQ      prometheus.Gauge
-        activeQ        prometheus.Gauge
-        maildropQ      prometheus.Gauge
-        deferredQ      prometheus.Gauge
-        holdQ          prometheus.Gauge
-        bounceQ        prometheus.Gauge
+	mutex     sync.RWMutex
+	totalQ    prometheus.Gauge
+	incomingQ prometheus.Gauge
+	activeQ   prometheus.Gauge
+	maildropQ prometheus.Gauge
+	deferredQ prometheus.Gauge
+	holdQ     prometheus.Gauge
+	bounceQ   prometheus.Gauge
 }
 
 // NewPostfixExporter returns an initialized Exporter.
 func NewPostfixExporter() *Exporter {
 	return &Exporter{
-          totalQ: prometheus.NewGauge(prometheus.GaugeOpts{
+		totalQ: prometheus.NewGauge(prometheus.GaugeOpts{
 			Namespace: namespace,
 			Name:      "total_queue_length",
 			Help:      "length of mail queue",
 		}),
-          incomingQ: prometheus.NewGauge(prometheus.GaugeOpts{
-                        Namespace: namespace,
-                        Name:      "incoming_queue_length",
-                        Help:      "length of incoming mail queue",
-                }),
-          activeQ: prometheus.NewGauge(prometheus.GaugeOpts{
-                        Namespace: namespace,
-                        Name:      "active_queue_length",
-                        Help:      "length of active mail queue",
-                }),
-          maildropQ: prometheus.NewGauge(prometheus.GaugeOpts{
-                        Namespace: namespace,
-                        Name:      "maildrop_queue_length",
-                        Help:      "length of maildrop queue",
-                }),
-          deferredQ: prometheus.NewGauge(prometheus.GaugeOpts{
-                        Namespace: namespace,
-                        Name:      "deferred_queue_length",
-                        Help:      "length of deferred mail queue",
-                }),
-          holdQ: prometheus.NewGauge(prometheus.GaugeOpts{
-                        Namespace: namespace,
-                        Name:      "hold_queue_length",
-                        Help:      "length of hold mail queue",
-                }),
-          bounceQ: prometheus.NewGauge(prometheus.GaugeOpts{
-                        Namespace: namespace,
-                        Name:      "bounce_queue_length",
-                        Help:      "length of bounce mail queue",
-                }),
-
+		incomingQ: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "incoming_queue_length",
+			Help:      "length of incoming mail queue",
+		}),
+		activeQ: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "active_queue_length",
+			Help:      "length of active mail queue",
+		}),
+		maildropQ: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "maildrop_queue_length",
+			Help:      "length of maildrop queue",
+		}),
+		deferredQ: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "deferred_queue_length",
+			Help:      "length of deferred mail queue",
+		}),
+		holdQ: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "hold_queue_length",
+			Help:      "length of hold mail queue",
+		}),
+		bounceQ: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "bounce_queue_length",
+			Help:      "length of bounce mail queue",
+		}),
 	}
 }
 
 // Describe describes all the metrics ever exported by the postfix exporter. It
 // implements prometheus.Collector.
 func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
-    e.totalQ.Describe(ch)
-    e.incomingQ.Describe(ch)
-    e.activeQ.Describe(ch)
-    e.maildropQ.Describe(ch)
-    e.deferredQ.Describe(ch)
-    e.holdQ.Describe(ch)
-    e.bounceQ.Describe(ch)
+	e.totalQ.Describe(ch)
+	e.incomingQ.Describe(ch)
+	e.activeQ.Describe(ch)
+	e.maildropQ.Describe(ch)
+	e.deferredQ.Describe(ch)
+	e.holdQ.Describe(ch)
+	e.bounceQ.Describe(ch)
 }
 
-func getPostfixQueueLength() string {
-    cmd := "/usr/sbin/postqueue -p | tail -n1 | awk '{print $5}'"
-    out, _ := exec.Command("bash", "-c", cmd).Output()
-    len := string(out)
-    len = strings.TrimSpace(len)
-    return len
-}
-
-func getQueueDir() string{
-    cmd := "postconf -h queue_directory"
-    out,_ := exec.Command("bash", "-c", cmd).Output()
-    dir := string(out)
-    dir = strings.TrimSpace(dir)
-    return dir
-}
-
-func getQueueLength(qname string, qdir string) string{
-    cmd := "sudo find "+qdir+"/"+qname+" -type f -print | wc -l | awk '{print $1}'"
-    out,_ := exec.Command("bash", "-c", cmd).Output()
-    qlen := string(out)
-    qlen = strings.TrimSpace(qlen)
-    return qlen
+func countDir(tgt string) (float64, error) {
+	root, err := os.Open(tgt)
+	if err != nil {
+		log.Fatal("[0] error opening %s: %v", tgt, err.Error())
+	}
+	res, err := root.Readdir(-1)
+	count := 0.0
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	for _, fi := range res {
+		if fi.IsDir() {
+			newtgt := fmt.Sprintf("%s/%s", tgt, fi.Name())
+			mycount, err := countDir(newtgt)
+			if err != nil {
+				log.Printf("error opening %s: %+v", newtgt, err)
+			}
+			count += mycount
+		} else {
+			count++
+		}
+	}
+	return count, nil
 }
 
 func (e *Exporter) scrape(ch chan<- prometheus.Metric) error {
-    queue_length, _ := strconv.ParseFloat(getPostfixQueueLength(), 64)
-    e.totalQ.Set(float64(queue_length))
-    queue_dir := getQueueDir()
-    
-    incoming_queue, _ := strconv.ParseFloat(getQueueLength("incoming", queue_dir), 64)
-    e.incomingQ.Set(float64(incoming_queue))
-   
-    active_queue, _ := strconv.ParseFloat(getQueueLength("active", queue_dir), 64)
-    e.activeQ.Set(float64(active_queue))
+	total_length := 0.0
 
-    maildrop_queue, _ := strconv.ParseFloat(getQueueLength("maildrop", queue_dir), 64)
-    e.maildropQ.Set(float64(maildrop_queue))
-   
-    deferred_queue, _ := strconv.ParseFloat(getQueueLength("deferred", queue_dir), 64)
-    e.deferredQ.Set(float64(deferred_queue))
+	incoming_queue, _ := countDir(fmt.Sprintf("%s/incoming", *queueDir))
+	e.incomingQ.Set(incoming_queue)
+	total_length += incoming_queue
 
-    hold_queue, _ := strconv.ParseFloat(getQueueLength("hold", queue_dir), 64)
-    e.holdQ.Set(float64(hold_queue))
-   
-    bounce_queue, _ := strconv.ParseFloat(getQueueLength("bounce", queue_dir), 64)
-    e.bounceQ.Set(float64(bounce_queue))
+	active_queue, _ := countDir(fmt.Sprintf("%s/active", *queueDir))
+	e.activeQ.Set(active_queue)
+	total_length += active_queue
 
-    return nil
+	maildrop_queue, _ := countDir(fmt.Sprintf("%s/maildrop", *queueDir))
+	e.maildropQ.Set(maildrop_queue)
+	total_length += maildrop_queue
+
+	deferred_queue, _ := countDir(fmt.Sprintf("%s/deferred", *queueDir))
+	e.deferredQ.Set(deferred_queue)
+	total_length += deferred_queue
+
+	hold_queue, _ := countDir(fmt.Sprintf("%s/hold", *queueDir))
+	e.holdQ.Set(hold_queue)
+	total_length += hold_queue
+
+	bounce_queue, _ := countDir(fmt.Sprintf("%s/bounce", *queueDir))
+	e.bounceQ.Set(bounce_queue)
+	total_length += bounce_queue
+
+	e.totalQ.Set(total_length)
+
+	return nil
 }
 
 // Collect fetches the stats of a user and delivers them
@@ -143,16 +149,16 @@ func (e *Exporter) scrape(ch chan<- prometheus.Metric) error {
 func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 	e.mutex.Lock() // To protect metrics from concurrent collects.
 	defer e.mutex.Unlock()
-        if err := e.scrape(ch); err != nil {
+	if err := e.scrape(ch); err != nil {
 		log.Printf("Error scraping postfix: %s", err)
 	}
-        e.totalQ.Collect(ch)
-        e.incomingQ.Collect(ch)
-        e.activeQ.Collect(ch)
-        e.maildropQ.Collect(ch)
-        e.deferredQ.Collect(ch)
-        e.holdQ.Collect(ch)
-        e.bounceQ.Collect(ch)
+	e.totalQ.Collect(ch)
+	e.incomingQ.Collect(ch)
+	e.activeQ.Collect(ch)
+	e.maildropQ.Collect(ch)
+	e.deferredQ.Collect(ch)
+	e.holdQ.Collect(ch)
+	e.bounceQ.Collect(ch)
 	return
 }
 
@@ -162,8 +168,8 @@ func main() {
 	exporter := NewPostfixExporter()
 	prometheus.MustRegister(exporter)
 	http.Handle(*metricsPath, prometheus.Handler())
-        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	    w.Write([]byte(`<html>
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
                 <head><title>Postfix exporter</title></head>
                 <body>
                    <h1>Postfix exporter</h1>
